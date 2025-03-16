7885afe85fa0e2d21c5fdf4a6f880ed9a7e3e64b
nallabkumar@gmail.com <nallabkumar@gmail.com>
2025-03-16 10:12:59 +0530

    adding variables

--- first.sh
+++ first.sh
@@ -1,10 +1,8 @@
-echo "first"
-echo "second"
-echo "third"
-echo "first"
-echo "second"
-echo "third"
-echo "first"
-echo "second"
-echo "third"
-echo "fourth"
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "PERSON1 : Hi How are you PERSON2"
+echo "PERSON2 : Im fine how  about you PERSON2"
+echo "PERSON1 : im fine PERSON2"
