e5b3e31d6a642ea054ce121c7ae7e90832740f62
nallabkumar@gmail.com <nallabkumar@gmail.com>
2025-03-16 10:27:44 +0530

    adding variables in diff way2

--- second.sh
+++ second.sh
@@ -3,6 +3,6 @@
 PERSON1=Ramesh
 PERSON2=Suresh
 
-echo "${PERSON1} : Hi How are you PERSON2"
-echo "${PERSON2} : Im fine how  about you PERSON2"
-echo "${PERSON1} : im fine PERSON2"
+echo "${PERSON1} : Hi How are you ${PERSON2}"
+echo "${PERSON2} : Im fine how  about you ${PERSON2}"
+echo "${PERSON1} : im fine ${PERSON2}"
